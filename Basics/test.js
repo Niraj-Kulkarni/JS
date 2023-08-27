e2a73eb5e3eff6d4105a68cfc0ecd370e51cb0d6
Niraj Kulkarni <78420588+Niraj-Kulkarni@users.noreply.github.com>
2023-08-27 07:02:30 +0000

    datatypes

--- Basics/test.js
+++ Basics/test.js
@@ -1,1 +1,6 @@
-console.log("Hello World");
+"use strict" //After using this statement all your JS code will be treated as newer version of JS 
+const accountId = 74592
+let accountEmail = "niraj@gmail.com"
+var accountPassword = "12334"
+ let accountCity = "Pune"
+console.table([accountId, accountPassword, accountEmail, accountCity])

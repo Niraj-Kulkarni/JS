94b34efc258b8fe70057a9244a1c4a9e0751a6ef
Niraj Kulkarni <78420588+Niraj-Kulkarni@users.noreply.github.com>
2023-09-08 09:42:37 +0000

    concatenating datatypes

--- Basics/test.js
+++ Basics/test.js
@@ -1,6 +1,12 @@
 "use strict" //After using this statement all your JS code will be treated as newer version of JS 
-const accountId = 74592
-let accountEmail = "niraj@gmail.com"
-var accountPassword = "12334"
- let accountCity = "Pune"
-console.table([accountId, accountPassword, accountEmail, accountCity])
+// const accountId = 74592
+// let accountEmail = "niraj@gmail.com"
+// var accountPassword = "12334"
+// let accountCity = "Pune"
+// console.table([accountId, accountPassword, accountEmail, accountCity])
+// console.log("hello");
+let str1 = "3"
+let str2 = " Niraj"
+let str3 = str1 + str2
+console.log(typeof(str3));
+console.log(str3);
